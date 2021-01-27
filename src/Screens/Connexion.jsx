5231e0485be54d75625843e3e220b99c7b62c853
William Pepin <wpepin00@gmail.com>
2021-01-26 20:34:35 -0500

    Création du formulaire de connexion.

---
+++ src/Screens/Connexion.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import * as Yup from "yup";
+import Form from "../Components/Forms/Form";
+import Field from "../Components/Forms/Field";
+import SubmitButton from "../Components/Forms/SubmitButton";
+
+export default function Connexion() {
+  const initialValues = { email: "", password: "" };
+  const onSubmit = (values, actions) => {
+    // TODO Faire le onSubmit call à l'API
+    alert(JSON.stringify(values, null, 2));
+  };
+  const validationSchema = Yup.object().shape({
+    email: Yup.string().required().min(4).email().max(127).label("Courriel"),
+    password: Yup.string().required().min(8).max(64).label("Mot de passe"),
+  });
+
+  return (
+    <Form
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      validationSchema={validationSchema}
+    >
+      <Field
+        name="email"
+        label={"Courriel"}
+        placeholder="exemple@courriel.com"
+      />
+      <Field
+        type="password"
+        name="password"
+        label={"Mot de passe"}
+        placeholder="**************"
+      />
+      <SubmitButton title="Soumettre"></SubmitButton>
+    </Form>
+  );
+}
