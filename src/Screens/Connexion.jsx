a6b0872167b0de60cef44992f51e453836f56ace
William Pepin <wpepin00@gmail.com>
2021-01-27 07:06:17 -0500

    Conception visuel du formulaire de connexion et ajout des validations Yup.

--- src/Screens/Connexion.jsx
+++ src/Screens/Connexion.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import * as Yup from "yup";
+
 import Form from "../Components/Forms/Form";
 import Field from "../Components/Forms/Field";
 import SubmitButton from "../Components/Forms/SubmitButton";
@@ -16,23 +18,38 @@ export default function Connexion() {
   });
 
   return (
-    <Form
-      initialValues={initialValues}
-      onSubmit={onSubmit}
-      validationSchema={validationSchema}
-    >
-      <Field
-        name="email"
-        label={"Courriel"}
-        placeholder="exemple@courriel.com"
-      />
-      <Field
-        type="password"
-        name="password"
-        label={"Mot de passe"}
-        placeholder="**************"
-      />
-      <SubmitButton title="Soumettre"></SubmitButton>
-    </Form>
+    <div className="card col-xl-4 col-md-8 col-sm-10" style={styles.card}>
+      <Form
+        initialValues={initialValues}
+        onSubmit={onSubmit}
+        validationSchema={validationSchema}
+      >
+        <Field
+          name="email"
+          label={"Courriel"}
+          placeholder="exemple@courriel.com"
+        />
+        <Field
+          type="password"
+          name="password"
+          label={"Mot de passe"}
+          placeholder="**************"
+        />
+        <SubmitButton title="Se connecter"></SubmitButton>
+      </Form>
+      <Link style={styles.link} to="/inscription">
+        S'inscrire
+      </Link>
+    </div>
   );
 }
+
+const styles = {
+  card: {
+    padding: "2rem",
+    margin: "25vh auto",
+  },
+  link: {
+    paddingLeft: "14px",
+  },
+};
