bfe9e5f95e10bc0d6f0679c403059b9a1c90cf3a
William Pepin <wpepin00@gmail.com>
2021-02-02 10:31:16 -0500

    Ajout des dépendances de l'api dans le projet

---
+++ api/index.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const tasks = require("./routes/tasks");
+const users = require("./routes/users");
+const auth = require("./routes/auth");
+const app = express();
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, x-auth-token, Content-Type, Accept"
+  );
+  next();
+});
+
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
+
+app.use(bodyParser.json());
+app.use("/api/users", users);
+app.use("/api/tasks", tasks);
+app.use("/api/auth", auth);
