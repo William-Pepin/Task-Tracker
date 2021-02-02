6f2d001269503175ac396f4768a1746c3090a829
William Pepin <wpepin00@gmail.com>
2021-02-02 10:47:33 -0500

    Ajout de la logique de connexion et deconnexion dans l'app

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,45 @@
+import React from "react";
 import { Route, Redirect, Switch } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import jwtDecode from "jwt-decode";
 
 import Connexion from "./Screens/Connexion";
 import Inscription from "./Screens/Inscription";
 import TableauDeBord from "./Screens/TableauDeBord";
-import Equipe from "./Screens/Equipe";
 import Invalide from "./Screens/Invalide";
+import Logout from "./Components/Logout";
 
-export default function App() {
-  const connected = false; // todo
-  return (
-    <div className="App">
-      <Switch>
-        <Route path="/connexion" component={Connexion} />
-        <Route path="/inscription" component={Inscription} />
-        <Route path="/accueil" component={TableauDeBord} />
-        <Route path="/equipes/:id" component={Equipe} />
-        <Route path="/404" component={Invalide} />
-        <Redirect path="/" exact to={connected ? "/accueil" : "/connexion"} />
-        <Redirect to="/404" />
-      </Switch>
-    </div>
-  );
+export default class App extends React.Component {
+  state = {
+    user: () => {
+      jwtDecode(localStorage.getItem("token"));
+    },
+  };
+  componentDidMount() {
+    try {
+      const jwt = localStorage.getItem("token");
+      const user = jwtDecode(jwt);
+      this.setState({ user });
+    } catch (e) {}
+  }
+  render() {
+    const connected = localStorage.getItem("token");
+    return (
+      <div className="App">
+        <ToastContainer />
+        <Switch>
+          <Route path="/connexion" component={Connexion} />
+          <Route path="/inscription" component={Inscription} />
+          <Route path="/logout" component={Logout} />
+          <Route
+            path="/accueil"
+            component={() => <TableauDeBord user={this.state.user} />}
+          />
+          <Route path="/404" component={Invalide} />
+          <Redirect path="/" exact to={connected ? "/accueil" : "/connexion"} />
+          <Redirect to="/404" />
+        </Switch>
+      </div>
+    );
+  }
 }
