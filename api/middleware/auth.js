58753364f666624251af0a9e2fb614fca920fb7c
William Pepin <wpepin00@gmail.com>
2021-02-05 09:17:56 -0500

    Ajout des commentaires afin d'expliquer le code de l'API

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -1,6 +1,13 @@
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
+
+/**
+ * Fonction utilisé comme Middleware Espress, utilisé pour authentifier un utilisateur de l'application à l'aide de son JSON WEB TOKEN. le JsonWebToken est dans le header "x-auth-token". Décode le jeton et l'utilise pour accéder à l'usager. Les paramètres 
+ * @param {*} req requête http de l'usager
+ * @param {*} res réponse de l'api
+ * @param {*} next prochaine fonction à exécuter suite à l'authentification 
+ */
 function auth(req, res, next) {
   const token = req.header("x-auth-token");
   if (!token) return res.status(401).send("Access denied. No token provided.");
